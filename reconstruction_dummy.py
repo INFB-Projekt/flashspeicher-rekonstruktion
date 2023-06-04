60c016718e578ae7c6ac634caa4fc5e04590c612
Tobias Fernberg <tobias.fernberg@web.de>
2023-06-04 17:44:51 +0200

    Code Refactoring (#7)

--- reconstruction_dummy.py
+++ reconstruction_dummy.py
@@ -1,76 +1,83 @@
 import mmap
-import contextlib
 import os
 import csv
 import datetime
+import argparse
 
 
-reconstruktionPath = "E:\\Vmshare"
-csvPath = "E:\\Vmshare\\CSV"
-imageName = "sdCard.img"
+parser = argparse.ArgumentParser(description="Script to reconstruct data from a CSV file ")
+parser.add_argument("--reconstruction-path", required=True, help="Path to the desired destination")
+parser.add_argument("--csv-path", required=True, help="Path to the csv file")
+parser.add_argument("--image-name", required=True, help="Name of the image")
+args = parser.parse_args()
+reconstruction_path = args.reconstruction_path
+csv_path = args.csv_path
+image_name = args.image_name
 
-f = open(os.path.join(reconstruktionPath, imageName), "r+b")
+
+f = open(os.path.join(reconstruction_path, image_name), "r+b")
 mm = mmap.mmap(f.fileno(), 0)
 
-dateString = "18.05.2023"
-timeString = "14:22:02"
 
-dateObj = datetime.datetime.strptime(dateString, "%d.%m.%Y")
-timeObj = datetime.datetime.strptime(timeString, "%H:%M:%S")
-datetimeObj = datetime.datetime.combine(dateObj.date(), timeObj.time())
-rekostructionTimestamp = int(datetimeObj.timestamp()) * 1000
+date_string = "18.05.2023"
+time_string = "14:22:02"
+
+date_obj = datetime.datetime.strptime(date_string, "%d.%m.%Y")
+time_obj = datetime.datetime.strptime(time_string, "%H:%M:%S")
+datetime_obj = datetime.datetime.combine(date_obj.date(), time_obj.time())
+rekostruction_timestamp = int(datetime_obj.timestamp()) * 1000
 
 #find correct csv file
-targetTrace = ""
+target_trace = ""
 for trace in os.listdir("CSV"):
-    traceTimestamp = int(trace[:-4])
-    if(rekostructionTimestamp > traceTimestamp):
-        targetTrace = trace
+    trace_timestamp = int(trace[:-4])
+    if(rekostruction_timestamp > trace_timestamp):
+        target_trace = trace
     else: break
 
 #find correct row in csv file
-targetRow = 0
-with open(f"CSV\\{targetTrace}", "r") as csvfile:
+target_row = 0
+with open(f"CSV\\{target_trace}", "r") as csvfile:
     reader = csv.reader(csvfile, delimiter=";")
     next(reader) #TODO remember to delete (maybe)
 
     for row in reader:
-        currentRowTimeStamp = int(targetTrace[:-4]) + float(row[0]) * 1000
-        if(rekostructionTimestamp > currentRowTimeStamp):
-            targetRow += 1           
+        current_row_timestamp = int(target_trace[:-4]) + float(row[0]) * 1000
+        if(rekostruction_timestamp > current_row_timestamp):
+            target_row += 1           
         else:
             break
     print("Reconstructing until the following point:")
-    print(datetime.datetime.fromtimestamp(currentRowTimeStamp / 1000))
+    print(datetime.datetime.fromtimestamp(current_row_timestamp / 1000))
     csvfile.close()
-print("Target Trace: " + targetTrace)
-print("Target Row: "+ targetRow)
+print("Target Trace: " + target_trace)
+print("Target Row: "+ target_row)
 
 #Main reconstruction function
-def reconstruction(csvName, rowCount):
-    with open(f'CSV\\{csvName}', 'r') as csvfile:
+def reconstruction(csv_name, row_count):
+    with open(f'CSV\\{csv_name}', 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=";")
         next(reader) #TODO remember to delete (maybe)
-        currentRow = 0
+        current_row = 0
     
         for row in reader:
-            if(row[1] == "0x58" and (rowCount == -1 or currentRow < rowCount)):
+            if(row[1] == "0x58" and (row_count == -1 or current_row < row_count)):
                 mm.seek(int(row[2], 0))
                 hex_bytes = bytes.fromhex(row[3][2:])
                 mm.write(hex_bytes)
             else: 
-                print(currentRow)
+                print(current_row)
                 break
-            currentRow += 1
+            current_row += 1
         csvfile.close()
 
 
 #Either reconstruct everything in file or only until last row
-for csvName in os.listdir("CSV"):
-    if(targetTrace != csvName):
-        reconstruction(csvName, -1)
+for csv_name in os.listdir("CSV"):
+    if(target_trace != csv_name):
+        reconstruction(csv_name, -1)
     else:
-        reconstruction(csvName, targetRow)
+        reconstruction(csv_name, target_row)
         break
 
 f.close()
