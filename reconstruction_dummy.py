18da6a3219f467adc8e65959b089ae5e3e461bf2
TobiGit11 <92452494+TobiGit11@users.noreply.github.com>
2023-06-09 21:08:13 +0200

    Add tests for reconstruction_dummy (#15) (#18)
    * Add Script (#9)
    
    * Add script (#9)
    
    * CR Changes (#9)
    
    * CR Changes (#9)
    
    * Add script (#7)
    
    * Add script (#7)
    
    * Code Refactoring (#7)
    
    * Refactor reconstruction_dummy and add Tests
    
    * Move test input files to resources folder (#15)
    
    * Update test_reconstruction_dummy due to files move (#15)
    
    * Add Tests section to README (#15)
    
    * Add .gitignore (#15)
    
    * Remove pycache (#15)
    
    * Revert "Remove pycache (#15)"
    
    This reverts commit a5901acde68a003a5a018fc975a256fe01f8a7de.
    
    ---------
    
    Co-authored-by: foxx08 <92452678+foxx08@users.noreply.github.com>
    Co-authored-by: foxx08 <samsepiol08@protonmail.com>

--- reconstruction_dummy.py
+++ reconstruction_dummy.py
@@ -5,19 +5,14 @@ import datetime
 import argparse
 
 
-parser = argparse.ArgumentParser(description="Script to reconstruct data from a CSV file ")
-parser.add_argument("--reconstruction-path", required=True, help="Path to the desired destination")
-parser.add_argument("--csv-path", required=True, help="Path to the csv file")
-parser.add_argument("--image-name", required=True, help="Name of the image")
-args = parser.parse_args()
-reconstruction_path = args.reconstruction_path
-csv_path = args.csv_path
-image_name = args.image_name
-
-
-f = open(os.path.join(reconstruction_path, image_name), "r+b")
-mm = mmap.mmap(f.fileno(), 0)
-
+def process_arguments():
+    global image_path, csv_path
+    parser = argparse.ArgumentParser(description="Script to reconstruct data from a CSV file ")
+    parser.add_argument("--image-path", required=True, help="Path to image file")
+    parser.add_argument("--csv-path", required=True, help="Path to the csv file")
+    args = parser.parse_args()
+    image_path = args.image_path
+    csv_path = args.csv_path
 
 date_string = "18.05.2023"
 time_string = "14:22:02"
@@ -25,37 +20,51 @@ time_string = "14:22:02"
 date_obj = datetime.datetime.strptime(date_string, "%d.%m.%Y")
 time_obj = datetime.datetime.strptime(time_string, "%H:%M:%S")
 datetime_obj = datetime.datetime.combine(date_obj.date(), time_obj.time())
-rekostruction_timestamp = int(datetime_obj.timestamp()) * 1000
-
+reconstruction_timestamp = int(datetime_obj.timestamp()) * 1000
+print(reconstruction_timestamp)
 #find correct csv file
-target_trace = ""
-for trace in os.listdir("CSV"):
-    trace_timestamp = int(trace[:-4])
-    if(rekostruction_timestamp > trace_timestamp):
-        target_trace = trace
-    else: break
+def find_csv_file(csv_name_list):
+    global reconstruction_timestamp
+
+    target_csv = ""
+    if(reconstruction_timestamp < int(csv_name_list[0][:-4])):
+        raise Exception("Timestamp too early to reconstruct")
+    for trace in csv_name_list:
+        trace_timestamp = int(trace[:-4])
+        if(reconstruction_timestamp > trace_timestamp):
+            target_csv = trace
+        else: break
+    return target_csv
 
 #find correct row in csv file
-target_row = 0
-with open(f"CSV\\{target_trace}", "r") as csvfile:
-    reader = csv.reader(csvfile, delimiter=";")
-    next(reader) #TODO remember to delete (maybe)
+def find_target_row(target_csv, path_dest_csv):
+    target_row = 0
+    with open(path_dest_csv, "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=";")
+        next(reader) #TODO remember to delete (maybe)
 
-    for row in reader:
-        current_row_timestamp = int(target_trace[:-4]) + float(row[0]) * 1000
-        if(rekostruction_timestamp > current_row_timestamp):
-            target_row += 1           
-        else:
-            break
-    print("Reconstructing until the following point:")
-    print(datetime.datetime.fromtimestamp(current_row_timestamp / 1000))
-    csvfile.close()
-print("Target Trace: " + target_trace)
-print("Target Row: "+ target_row)
+        if(reconstruction_timestamp < int(target_csv[:-4])):
+            raise Exception("Timestamp too early")
+
+        for row in reader:
+            current_row_timestamp = int(target_csv[:-4]) + float(row[0]) * 1000
+            if(reconstruction_timestamp > current_row_timestamp):
+                target_row += 1           
+            else:
+                break
+        print("Reconstructing until the following point:")
+        print(datetime.datetime.fromtimestamp(current_row_timestamp / 1000))
+        csvfile.close()
+    print("Target Trace: " + str(target_csv))
+    print("Target Row: "+ str(target_row))
+    return target_row
 
 #Main reconstruction function
-def reconstruction(csv_name, row_count):
-    with open(f'CSV\\{csv_name}', 'r') as csvfile:
+def reconstruction(row_count, path_dest_csv):
+    f = open(image_path, "r+b")
+    mm = mmap.mmap(f.fileno(), 0)
+
+    with open(path_dest_csv, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=";")
         next(reader) #TODO remember to delete (maybe)
         current_row = 0
@@ -70,16 +79,27 @@ def reconstruction(csv_name, row_count):
                 break
             current_row += 1
         csvfile.close()
+    
+    f.close()
+    mm.close()
 
 
 #Either reconstruct everything in file or only until last row
-for csv_name in os.listdir("CSV"):
-    if(target_trace != csv_name):
-        reconstruction(csv_name, -1)
-    else:
-        reconstruction(csv_name, target_row)
-        break
 
-f.close()
-mm.close
+if __name__ == "__main__":
+    image_path = ""
+    csv_path = ""
+    
+    target_csv = find_csv_file(os.listdir(csv_path))
+
+    target_row = find_target_row(target_csv, f"{csv_path}\\{target_csv}")
+
+    process_arguments()
+
+    for csv_name in os.listdir(csv_path):
+        if(target_csv != csv_name):
+            reconstruction(-1, f'{csv_path}\\{csv_name}')
+        else:
+            reconstruction(target_row, f'{csv_path}\\{csv_name}')
+            break
 
