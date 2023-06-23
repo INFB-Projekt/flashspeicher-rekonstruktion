166cddb3a6e82d5bd674912293c9f8ca219ac63e
foxx08 <samsepiol08@protonmail.com>
2023-06-23 12:15:50 +0200

    Format file and convert variable to snake_case (#21)

--- tests/test_trace_reconstruction.py
+++ tests/test_trace_reconstruction.py
@@ -5,19 +5,20 @@ import filecmp
 import shutil
 
 
-
 @pytest.mark.parametrize("input_value, expected_result", [
     ("23-05-18T14_22_02S000", "23-05-18T14_22_01S000.csv"),
     ("23-05-15T19_56_47S000", pytest.raises(Exception)),
     ("23-05-18T19_56_47S000", "23-05-18T14_22_37S000.csv")
 ])
-
 def test_find_csv_file(input_value, expected_result):
-    csv__working_dummy_list = ["23-05-15T21_56_47S000.csv", "23-05-18T14_20_50S000.csv", "23-05-18T14_21_30S000.csv", "23-05-18T14_21_37S000.csv", "23-05-18T14_21_45S000.csv",
-                      "23-05-18T14_22_01S000.csv", "23-05-18T14_22_15S000.csv", "23-05-18T14_22_21S000.csv", "23-05-18T14_22_29S000.csv", "23-05-18T14_22_34S000.csv", "23-05-18T14_22_37S000.csv"]
-    trace_reconstruction.datetime_reconstruction_timestamp = datetime.datetime.strptime(input_value, "%y-%m-%dT%H_%M_%SS%f")
+    csv__working_dummy_list = ["23-05-15T21_56_47S000.csv", "23-05-18T14_20_50S000.csv", "23-05-18T14_21_30S000.csv",
+                               "23-05-18T14_21_37S000.csv", "23-05-18T14_21_45S000.csv",
+                               "23-05-18T14_22_01S000.csv", "23-05-18T14_22_15S000.csv", "23-05-18T14_22_21S000.csv",
+                               "23-05-18T14_22_29S000.csv", "23-05-18T14_22_34S000.csv", "23-05-18T14_22_37S000.csv"]
+    trace_reconstruction.datetime_reconstruction_timestamp = datetime.datetime.strptime(input_value,
+                                                                                        "%y-%m-%dT%H_%M_%SS%f")
 
-    if(input_value == "23-05-15T19_56_47S000"):
+    if (input_value == "23-05-15T19_56_47S000"):
         with pytest.raises(Exception):
             find_csv_file(csv__working_dummy_list)
     else:
@@ -31,11 +32,11 @@ def test_find_csv_file(input_value, expected_result):
     ("23-05-18T14_22_21S045", 19),
     ("23-05-18T15_22_21S025", 100)
 ])
-
 def test_find_target_row(input_value, expected_result):
-    trace_reconstruction.datetime_reconstruction_timestamp = datetime.datetime.strptime(input_value, "%y-%m-%dT%H_%M_%SS%f")
+    trace_reconstruction.datetime_reconstruction_timestamp = datetime.datetime.strptime(input_value,
+                                                                                        "%y-%m-%dT%H_%M_%SS%f")
     target_csv = "./tests/resources/23-05-18T14_22_21S025.csv"
-    if(input_value == "23-05-18T13_22_21S025"):
+    if (input_value == "23-05-18T13_22_21S025"):
         with pytest.raises(Exception):
             find_target_row("23-05-18T14_22_21S025.csv", target_csv)
     else:
@@ -44,30 +45,17 @@ def test_find_target_row(input_value, expected_result):
         assert result == expected_result
 
 
-
 def test_reconstruction():
-    
     csv = "./tests/resources/test_trace.csv"
     result = "./tests/resources/result_image.img"
-    copyImg = "./tests/resources/copyImg.img"
+    copy_img = "./tests/resources/copyImg.img"
 
-    shutil.copy("./tests/resources/orgImg.img", copyImg)
+    shutil.copy("./tests/resources/orgImg.img", copy_img)
 
-    trace_reconstruction.image_path = copyImg
+    trace_reconstruction.image_path = copy_img
 
     trace_reconstruction.reconstruction(-1, csv)
 
-    assert filecmp.cmp(copyImg, result)
+    assert filecmp.cmp(copy_img, result)
 
-    os.remove(copyImg)
-
-
-
-
-
-
-
-
-
-
-
+    os.remove(copy_img)
